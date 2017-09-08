d7c9d2f35a0f4baebd58b519ed0c2c812a3f7113
Arunkumar <larunkumar@arrisi.com>
2017-09-08 22:22:02 +0530

    Half way done with feature.Tested,so far so good.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+function aBignewFeature()
+{
+	console.log("Writing a lot of new code");
+	console.log("I am half way done");
+}
 function something()
 {
-	console.log("I am something")
+	console.log("I am something");
 }
-console.log("My first app!!!")
-something()
+console.log("My first app!!!");
+something();
+aBignewFeature();
